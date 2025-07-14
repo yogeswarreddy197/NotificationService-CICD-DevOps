7e611d173be4c60a2b6468b8904373179425de59
M YOGESWAR REDDY <yogeswar.22bce7540@vitapstudent.ac.in>
2025-07-15 01:56:13 +0530

    Added JUnit test file to test/ folder

---
+++ test/NotificationServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.notification;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NotificationServiceApplicationTests {
+    @Test
+    void testSendNotification() {
+        assertEquals("Notification sent", "Notification sent");
+    }
+}
